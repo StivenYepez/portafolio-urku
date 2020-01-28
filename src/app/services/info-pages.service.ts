83cd1a27d8c98f5c92a590b288a82ae554768181
Stiven Enrique Yepez <stiven.yepez.g@gmail.com>
2020-01-27 21:11:25 -0500

    insertando informacion desde firebase

--- src/app/services/info-pages.service.ts
+++ src/app/services/info-pages.service.ts
@@ -10,16 +10,33 @@ export class InfoPagesService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
+    this.cargarInfo();
+    this.cargarEquipo();
+    
+   }
 
+   private cargarInfo(){
     this.http.get('assets/data/data-pagian.json')
-              .subscribe( (resp: InfoPagina) => {
+    .subscribe( (resp: InfoPagina) => {
 
 
-                this.cargada = true;
-                this.info = resp;
-                console.log(resp);
-              });
+      this.cargada = true;
+      this.info = resp;
+     
+    });
+   }
+
+   private cargarEquipo(){
+    this.http.get('https://templai-stiv.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => {
+
+
+      this.equipo = resp;
+      console.log(resp);
+    });
    }
 }
